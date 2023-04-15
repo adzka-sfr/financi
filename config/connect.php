d965f053e6348c0ffa358b2900d79a990e744b02
Adzka SFR <adzka.sfr@gmail.com>
2023-04-15 11:28:50 +0700

    benerin koneksi

--- config/connect.php
+++ config/connect.php
@@ -3,8 +3,8 @@ session_start();
 date_default_timezone_set('Asia/Jakarta');
 $now = date('Y-m-d H:i:s');
 
-$connect = new mysqli("localhost", "root", "", "arto");
-// $connect = new mysqli("localhost", "kaad7559_fahmi", "rahmadiani", "kaad7559_financi");
+// $connect = new mysqli("localhost", "root", "", "arto");
+$connect = new mysqli("localhost", "kaad7559_fahmi", "rahmadiani", "kaad7559_financi");
 
 if (!$connect) {
     die("Connection failed" . mysqli_connect_error());
